698004376e3275419c3a1ad44d51dfd4d3fbd495
Esko Dijk <esko.dijk@iotconsultancy.nl>
2025-07-08 07:05:19 +0000

    BR build script with Python virtual env (venv)

---
+++ build_br.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script to activate the Python virtual env and build the OTBR.
+# Note: ./script/bootstrap.bash must be run before the first time build.
+
+set -euox pipefail
+
+activate_python_venv()
+{
+    if [[ ! -d .venv ]]; then
+        python3 -m venv .venv
+    fi
+    # shellcheck source=/dev/null
+    source .venv/bin/activate
+}
+
+activate_python_venv
+
+git submodule status
+export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb
+export GNUARMEMB_TOOLCHAIN_PATH=/tmp/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi/
+REFERENCE_PLATFORM=ncs REFERENCE_RELEASE_TYPE=1.4 ./script/make-reference-release.bash
